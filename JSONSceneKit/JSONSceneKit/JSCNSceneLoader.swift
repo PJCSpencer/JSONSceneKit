45632077de5294717758d2b629ccb994229f0c5b
Peter JC Spencer <pjc.spencer@gmail.com>
2018-04-19 14:14:20 +0100

    Added error catching support

--- JSONSceneKit/JSONSceneKit/JSCNSceneLoader.swift
+++ JSONSceneKit/JSONSceneKit/JSCNSceneLoader.swift
@@ -13,26 +13,31 @@ public class JSCNSceneLoader
 {
     func scene(withContentsAt path: String) -> SCNScene? /* TODO:Support update block/protocol ... */
     {
-        guard let jsonObject = try? JSONSerialization.collect(JSCNScene.Keys.all,
-                                                              contentsOf: path) else
-        { return nil }
+        do {
+            guard let jsonObject = try? JSONSerialization.collect(JSCNScene.Keys.all,
+                                                                contentsOf: path) else
+            { throw JSONSerializationError.json }
+            
+            let materials = jsonObject.JSONObjects(JSCNScene.Keys.material.rawValue)
+            let geometries = jsonObject.JSONObjects(JSCNScene.Keys.geometry.rawValue)
+            let nodes = jsonObject.JSONObjects(JSCNScene.Keys.node.rawValue)
+            
+            let scene: JSCNScene = JSCNScene()
+            scene.materialLib = SCNMaterial.library(jsonObjects: materials)
+            scene.geometryLib = SCNGeometry.library(jsonObjects: geometries)
+            scene.geometryLib.pass(scene.materialLib, jsonCollection: geometries)
+            
+            if let data = jsonObject.JSONObject(SCNScene.Keys.background.rawValue)
+            { scene.background.configure(jsonObject: data) }
+            
+            for jsonObject in nodes
+            { scene.addChildNode(jsonObject: jsonObject, to: scene.rootNode) }
+            
+            return scene
+        }
+        catch { print("WARNING:Possible incorrect json.") }
         
-        let materials = jsonObject.JSONObjects(JSCNScene.Keys.material.rawValue)
-        let geometries = jsonObject.JSONObjects(JSCNScene.Keys.geometry.rawValue)
-        let nodes = jsonObject.JSONObjects(JSCNScene.Keys.node.rawValue)
-        
-        let scene: JSCNScene = JSCNScene()
-        scene.materialLib = SCNMaterial.library(jsonObjects: materials)
-        scene.geometryLib = SCNGeometry.library(jsonObjects: geometries)
-        scene.geometryLib.pass(scene.materialLib, jsonCollection: geometries)
-        
-        if let data = jsonObject.JSONObject(SCNScene.Keys.background.rawValue)
-        { scene.background.configure(jsonObject: data) }
-        
-        for jsonObject in nodes
-        { scene.addChildNode(jsonObject: jsonObject, to: scene.rootNode) }
-        
-        return scene
+        return nil
     }
 }
 
